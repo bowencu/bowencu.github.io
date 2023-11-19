5822522cc57f60802291d1dd16e26e9e9ad44281
Bowen Cui <bowencui1221@gmail.com>
2023-11-18 21:03:51 -0800

    Upload files

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("Hello Hackers")
+console.log("Hello Hackers")
+
+let myname = "Nugget";
+console.log(myname)
+
+function say_hello(name) {
+    console.log("Hello, " + name + "!")
+}
+
+say_hello("Bowen")
+
+function increment() {
+    score_element = document.getElementById("score");
+    score_element.innerHTML = parseInt(score_element.innerHTML) + 1;
+}
